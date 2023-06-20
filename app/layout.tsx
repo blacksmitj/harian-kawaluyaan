764c6535888890a4ab3afab9f5b9fdfee83a1d8e
blacksmitj <andri.andreas.panjaitan@gmail.com>
2023-06-20 20:05:36 +0700

    adding manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,12 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#0C7066" />
+      {/* <!-- this sets logo in Apple smatphones. --> */}
+      <link rel="apple-touch-icon" href="/logo-72x72.png" />
+      {/* <!-- this sets the color of url bar in Apple smatphones --> */}
+      <meta name="apple-mobile-web-app-status-bar" content="#0C7066" />
       <body className={inter.className}>
         <div className="flex-1 mx-auto">{children}</div>
       </body>
